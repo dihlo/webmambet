4423fbc4e48c63947cca74e64ad407ada96a037d
Бугаев Андрей Андреевич <aa.bugaev@empl.s-vfu.ru>
2017-05-15 15:45:51 +0900

    PortfolioUnit beta complited

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from "./Header";
+import PortfolioUnit from './PortfolioUnit';
+import {Link, IndexLink} from 'react-router';
 
 function Portfolio(props) {
 	return (
 		<div className="PortfolioBG">
 			<Header />
 			<div>Portfolio</div>
+			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" />
+			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" />
+			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" />
+			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" />
+			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" />
 		</div>	
 	);
 }
--- src/components/PortfolioUnit.jsx
+++ src/components/PortfolioUnit.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
+import {Link, IndexLink} from 'react-router';
 
+function PortfolioUnit(props) {
+	return (
+		<div>
+			<a href={props.site_link}>
+				<div className="PortfolioUnitBlock">
+					<div className="PortfolioUnitImg"></div>	
+					<p>{props.site_name}</p>				
+				</div>
+			</a>
+		</div>
+	);
+}
 
+export default PortfolioUnit;
