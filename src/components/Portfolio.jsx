abf571c3bce58a28583abffe4c1bb28c7afbb20c
Бугаев Андрей Андреевич <aa.bugaev@empl.s-vfu.ru>
2017-05-18 17:34:46 +0900

    site beta complited

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,11 +7,11 @@ function Portfolio(props) {
 	return (
 		<div className="PortfolioBG">
 			<Header />
-			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" site_description_name="крутой сайт" site_description_des="Что о сdasdasd asdasdasfgasdfad asfsadasdasfasdsada sfasdasffas dsadasdasайте"/>
-			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" site_description_name="крутой сайт" site_description_des="Что о сайте"/>
-			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" site_description_name="крутой сайт" site_description_des="Что о сайте"/>
-			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" site_description_name="крутой сайт" site_description_des="Что о сайте"/>
-			<PortfolioUnit site_link="http://joker.ykt.ru/" site_name="joker.ykt.ru" site_description_name="крутой сайт" site_description_des="Что о сайте"/>
+			<PortfolioUnit site_link="http://gurme14.ru/" site_name="gurme14.ru" site_description_name="Гурмэ" site_description_des="Сеть фирменных гастраномов с домашней кухней"/>
+			<PortfolioUnit site_link="http://fitflakes.ru/" site_name="fitflakes.ru" site_description_name="FitFlakes" site_description_des="Производитель гранолы в г. Якутске"/>
+			<PortfolioUnit site_link="http://news.iltumen.ru/" site_name="news.iltumen.ru" site_description_name="Газета Илтумэн" site_description_des="Сайт парламентской газеты"/>
+			<PortfolioUnit site_link="http://yrokb.ru/" site_name="yrokb.ru" site_description_name="Офтальмологическая клиника" site_description_des="Якутская Республиканская офтальмологическая клиническая больница"/>
+			<PortfolioUnit site_link="http://glaz14.ru/" site_name="glaz14.ru" site_description_name="Офтальмологическая кабинет" site_description_des=""/>
 		</div>	
 	);
 }
